42acc4caa68a7b8b3cdc4db9e00dfcdb688ea108
Ismael Fuentes <ismafuentes@hotmail.es>
2021-06-09 16:21:51 +0200

    new sharpen filter + changes

--- Postprocessing/filters/Filter.cs
+++ Postprocessing/filters/Filter.cs
@@ -142,6 +142,86 @@ namespace Postprocessing.filters
 
             var sharpen = (Bitmap)source.Clone();
 
+            const int filterSize = 5;//5;
+
+            int w = sharpen.Width;
+            int h = sharpen.Height;
+
+            double[,] filter = new double[filterSize, filterSize]
+            {
+                {  0, -1, -1, -1,  0 },
+                { -1,  2,  2,  2, -1 },
+                { -1,  2,  16, 2, -1 },
+                { -1,  2,  2,  2, -1 },
+                {  0, -1, -1, -1,  0 }
+            };
+
+            double bias = 1 - strength;
+            double factor = strength / 16.0;
+
+            Color[,] result = new Color[w, h];
+            var data = sharpen.LockBits(new Rectangle(0, 0, w, h), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int bytes = data.Stride * h;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(data.Scan0, rgbValues, 0, bytes);
+
+            int rgb;
+
+            for (int x = 0; x < w; x++)
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    double rColor = 0.0;
+                    double gColor = 0.0;
+                    double bColor = 0.0;
+
+                    for (int fx = 0; fx < filterSize; fx++)
+                    {
+                        for (int fy = 0; fy < filterSize; fy++)
+                        {
+                            int imageX = (x - filterSize / 2 + fx + w) % w;
+                            int imageY = (y - filterSize / 2 + fy + h) % h;
+
+                            rgb = imageY * data.Stride + 3 * imageX;
+
+                            rColor += rgbValues[rgb + 2] * filter[fx, fy];
+                            gColor += rgbValues[rgb + 1] * filter[fx, fy];
+                            bColor += rgbValues[rgb + 0] * filter[fx, fy];
+                        }
+
+                        int r = Math.Min(Math.Max((int)(factor * rColor + bias), 0), 255);
+                        int g = Math.Min(Math.Max((int)(factor * gColor + bias), 0), 255);
+                        int b = Math.Min(Math.Max((int)(factor * bColor + bias), 0), 255);
+
+                        result[x, y] = Color.FromArgb(r, g, b);
+                    }
+
+                    rgb = y * data.Stride + 3 * x;
+
+                    rgbValues[rgb + 2] = result[x, y].R;
+                    rgbValues[rgb + 1] = result[x, y].G;
+                    rgbValues[rgb + 0] = result[x, y].B;
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, data.Scan0, bytes);
+
+            sharpen.UnlockBits(data);
+
+            return sharpen;
+        }
+
+        public Bitmap Sharpen_old(double strength)
+        {
+            if (strength < 0 || strength > 1)
+            {
+                throw new ArgumentOutOfRangeException("strength should be between 0 and 1");
+            }
+
+            var sharpen = (Bitmap)source.Clone();
+
             const int filterW = 5;
             const int filterH = 5;
 
